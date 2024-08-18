5919a5788847d3cc78ae9efd0951d9d7177430db
Ron Talman <ron@somethingwithcomputers.com>
2024-08-18 18:33:04 +0200

    Minor upgrades to the setup and pyproject

--- setup.py
+++ setup.py
@@ -5,17 +5,17 @@
 from setuptools import setup
 
 setup(
-    name="pixoo",
-    version="0.9.0",
-    author="Ron Talman",
-    description="A library to easily communicate with the Divoom Pixoo 64",
-    license="CC BY-NC-SA",
-    keywords="pixoo, divoom, pixoo64",
-    url="https://github.com/SomethingWithComputers/pixoo#readme",
+    name='pixoo',
+    version='0.9.0',
+    author='Ron Talman',
+    description='A library to easily communicate with the Divoom Pixoo 64',
+    license='CC BY-NC-SA',
+    keywords=['pixoo', 'divoom', 'pixoo64'],
+    url='https://github.com/SomethingWithComputers/pixoo#readme',
     packages=['pixoo'],
     project_urls={
-        "Issue Tracker": "https://github.com/SomethingWithComputers/pixoo/issues",
-        "Source": "https://github.com/SomethingWithComputers/pixoo"
+        'Issue Tracker': 'https://github.com/SomethingWithComputers/pixoo/issues',
+        'Source': 'https://github.com/SomethingWithComputers/pixoo'
     },
     install_requires=[
         'Flask ~= 3.0.3',
